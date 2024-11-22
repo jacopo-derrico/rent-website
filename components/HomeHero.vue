6cc608fe072228c9170ce8e3ebfa3b8359e38a68
jacopo-derrico <derricoj.design@gmail.com>
2024-11-22 17:59:31 +0100

    feat: add hero component w/style

---
+++ components/HomeHero.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="mt-24 lg:w-5/6 mx-auto">
+        <figure class="relative">
+            <div class="absolute z-10 left-0 right-0 text-white top-32">
+                <h1 class="text-center text-5xl font-semibold">
+                    Rent Anything, Anytime: <br>
+                    From Bikes to Cars, Directly from Owners
+                </h1>
+                <h4 class="text-center text-">
+                    Visionary Early-Stage Startup. Built with community feedback and powered by your insights
+                </h4>
+                <div class="mt-14 flex w-full justify-center gap-4">
+                    <NuxtLink class="bg-[#FFC436] text-black hover:bg-[#0174BE] hover:text-white py-3 px-5 rounded-xl cursor-pointer" to="/">
+                        Join as an Early adopter
+                    </NuxtLink>
+                    <NuxtLink class="bg-[#0174BE] hover:bg-[#FFC436] hover:text-white py-3 px-5 rounded-xl cursor-pointer" to="/">
+                        Watch the pitch deck
+                    </NuxtLink>
+                </div>
+            </div>
+            <div class="w-full h-full bg-[#0174BE] absolute rounded-2xl opacity-50"></div>
+            <img src="../assets/img/headlight.webp" alt="car light closeup">
+        </figure>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        
+        <HomeHero />
     </div>
 </template>
