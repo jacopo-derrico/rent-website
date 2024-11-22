d304a2448a5ff8a99b3c65853f4b6cd590dcfab2
jacopo-derrico <derricoj.design@gmail.com>
2024-11-22 16:31:59 +0100

    feat: add header w/style

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,60 @@
 <template>
-    <div>
-        head
+    <div class="flex justify-between items-center lg:w-5/6 mx-auto py-5">
+        <div>
+            <NuxtLink to="/">
+                <AppLogo class="w-[100px]"/>
+            </NuxtLink>
+        </div>
+        <ul class="flex items-center gap-4">
+            <li>
+                <NuxtLink class="linkAnimation" to="/">
+                    What we do
+                </NuxtLink>
+            </li>
+            <li>
+                <NuxtLink class="linkAnimation" to="/">
+                    FAQ
+                </NuxtLink>
+            </li>
+            <li>
+                <NuxtLink class="linkAnimation" to="/">
+                    Team
+                </NuxtLink>
+            </li>
+            <li>
+                <NuxtLink class="bg-[#FFC436] hover:bg-[#0174BE] hover:text-white p-3 rounded-xl cursor-pointer" to="/">
+                    Join as an Early adopter
+                </NuxtLink>
+            </li>
+        </ul>
     </div>
 </template>
+
+<style>
+    .linkAnimation {
+        position: relative;
+        /* color: #FFF;
+        text-decoration: none; */
+    }
+
+    .linkAnimation:before {
+        content: "";
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        bottom: -5px;
+        left: 0;
+        background-color: #FFC436;
+        visibility: hidden;
+        -webkit-transform: scaleX(0);
+        transform: scaleX(0);
+        -webkit-transition: all 0.3s ease-in-out 0s;
+        transition: all 0.3s ease-in-out 0s;
+    }
+
+    .linkAnimation:hover:before {
+        visibility: visible;
+        -webkit-transform: scaleX(1);
+        transform: scaleX(1);
+    }
+</style>
