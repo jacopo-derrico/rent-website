a1a9267515549c48a7f5ea0497554a17b709454f
jacopo-derrico <derricoj.design@gmail.com>
2024-11-22 17:06:51 +0100

    feat: basic footer w/style

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
-        
+    <div class="bg-[#0174BE] text-center text-white pt-10">
+        <div>
+            <div>
+                icon icon icon
+            </div>
+            <a href="/mailto:">infomail.com</a>
+        </div>
+        <div class="pt-10 pb-2">
+            2024 Omni Rent | <a href="">Privacy Policy</a> - <a href="">Cookie Policy</a>
+        </div>
     </div>
 </template>
