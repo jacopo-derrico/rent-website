1c7a43a6cc6b3f9736a85ff6005b837e7304c942
jacopo-derrico <derricoj.design@gmail.com>
2024-11-27 14:30:43 +0100

    feat: rent journey section

---
+++ components/HomeJourney.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="journey" class="w-full">
+        <div class=" py-10 bg-[#ffc336e3]">
+            <div class="flex lg:w-5/6 mx-auto">
+                <div class="w-1/2 p-5 flex gap-3">
+                    <div class="mt-7">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="M440-800v487L216-537l-56 57 320 320 320-320-56-57-224 224v-487h-80Z"/></svg>
+                    </div>
+                    <div class="text-white">
+                        <div class="bg-[#0174BE] rounded-lg p-4 flex items-center gap-4 mb-3 h-[80px]">
+                            <p class="text-3xl font-bold">1</p>
+                            <p class="text-xl font-semibold">Fill Out the Early Adopter Form</p>
+                        </div>
+                        <div class="bg-[#0174BE] rounded-lg p-4 flex items-center gap-4 mb-3 h-[80px]">
+                            <p class="text-3xl font-bold">2</p>
+                            <p class="text-xl font-semibold">Join Our Community</p>
+                        </div>
+                        <div class="bg-[#0174BE] rounded-lg p-4 flex items-center gap-4 mb-3 h-[80px]">
+                            <p class="text-3xl font-bold">3</p>
+                            <p class="text-xl font-semibold">Be the First to Test<br>
+                                the Prototype Upon Release</p>
+                        </div>
+                        <div class="bg-[#0174BE] rounded-lg p-4 flex items-center gap-4 mb-3 h-[80px]">
+                            <p class="text-3xl font-bold">4</p>
+                            <p class="text-xl font-semibold">Share Feedback and<br>
+                                Help Us Improve</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center w-1/2 p-5 flex flex-col justify-center">
+                    <h6 class="text-3xl font-bold mb-7">How to Join the Omni Rent Journey:</h6>
+                    <p class="text-lg">Exclusive perks await our early adopters—be part of the group that leads the way!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    #journey{
+        background-image: url(../assets/img/bicycle.webp);
+        background-position: center;
+        background-size: cover;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,6 @@
     <div>
         <HomeHero />
         <HomeComments />
+        <HomeJourney />
     </div>
 </template>
