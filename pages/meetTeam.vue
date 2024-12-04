f830d5bfb3049756c9cf02a245633648ca3e1213
jacopo-derrico <derricoj.design@gmail.com>
2024-12-04 17:39:09 +0100

    feat: add team page

---
+++ pages/meetTeam.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mt-36 lg:w-2/3 mx-auto">
+        <h1 class="text-center text-3xl font-semibold">
+            Meet our team
+        </h1>
+        <div>
+            <div class="relative w-fit">
+                <figure class="w-[200px] aspect-square bg-slate-200 rounded-2xl">
+                    <img src="" alt="">
+                </figure>
+                <div class="absolute top-1/3 -right-32">
+                    <h3 class="font-bold text-2xl text-[#0174BE]">Name surname</h3>
+                    <h6 class="font-medium text-lg">Role</h6>
+                    <div class="bg-[#FFC436] h-[2px] w-[65px]"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
