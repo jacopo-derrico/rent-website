77c9321aacdf03a77966b68357634b2dca7a7e4e
jacopo-derrico <derricoj.design@gmail.com>
2024-12-10 15:06:38 +0100

    feat: add email link

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -4,7 +4,7 @@
             <div>
                 icon icon icon
             </div>
-            <a href="/mailto:">infomail.com</a>
+            <a class="hover:underline" href="mailto:info@omnirent.io">info@omnirent.io</a>
         </div>
         <div class="pt-10 pb-2">
             2024 Omni Rent | <a href="">Privacy Policy</a> - <a href="">Cookie Policy</a>
