46e505e9e8c2765efef2b74487172eea420a58fa
jacopo-derrico <derricoj.design@gmail.com>
2024-12-17 16:41:13 +0100

    refactor: test SEO

--- pages/index.vue
+++ pages/index.vue
@@ -8,24 +8,25 @@
 </template>
 
 <script setup>
-    // useSeoMeta({
-    //     title: 'Omni Rent | Rent any vehicle, from bikes to cars',
-    //     ogTitle: 'Omni Rent | Rent any vehicle, from bikes to cars',
-    //     description: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!',
-    //     ogDescription: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!',
-    //     ogImage: '/public/og_image.png',
-    //     twitterCard: 'summary_large_image',
-    // })
-    definePageMeta({
+    useSeoMeta({
         title: 'Omni Rent | Rent any vehicle, from bikes to cars',
-        meta: [
-            { name: 'description', content: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!' },
-            { name: 'og:title', content: 'Omni Rent | Rent any vehicle, from bikes to cars' },
-            { name: 'og:description', content: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!' },
-            { name: 'og:type', content: 'website' },
-            { name: 'og:image', content: 'https://rent-website.netlify.app/og_image.png' },
-            { name: 'og:url', content: 'https://rent-website.netlify.app' },
-            { name: 'og:site_name', content: 'Omni Rent' },
-        ],
+        ogTitle: 'Omni Rent | Rent any vehicle, from bikes to cars',
+        description: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!',
+        ogDescription: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!',
+        ogImage: 'https://rent-website.netlify.app/og_image.png',
+        ogUrl: 'https://rent-website.netlify.app',
+        ogType: 'website'
     })
+    // useHead({
+    //     title: 'Omni Rent | Rent any vehicle, from bikes to cars',
+    //     meta: [
+    //         { name: 'description', content: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!' },
+    //         { name: 'og:title', content: 'Omni Rent | Rent any vehicle, from bikes to cars' },
+    //         { name: 'og:description', content: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!' },
+    //         { name: 'og:type', content: 'website' },
+    //         { name: 'og:image', content: 'https://rent-website.netlify.app/og_image.png' },
+    //         { name: 'og:url', content: 'https://rent-website.netlify.app' },
+    //         { name: 'og:site_name', content: 'Omni Rent' },
+    //     ],
+    // })
 </script>
