bfb60ae39992d4a2bfde5c49fbc8beb4f316b236
jacopo-derrico <derricoj.design@gmail.com>
2024-12-17 15:12:03 +0100

    refactor: useHead instead of useSeoMeta

--- pages/index.vue
+++ pages/index.vue
@@ -8,12 +8,24 @@
 </template>
 
 <script setup>
-    useSeoMeta({
+    // useSeoMeta({
+    //     title: 'Omni Rent | Rent any vehicle, from bikes to cars',
+    //     ogTitle: 'Omni Rent | Rent any vehicle, from bikes to cars',
+    //     description: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!',
+    //     ogDescription: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!',
+    //     ogImage: '/public/og_image.png',
+    //     twitterCard: 'summary_large_image',
+    // })
+    useHead({
         title: 'Omni Rent | Rent any vehicle, from bikes to cars',
-        ogTitle: 'Omni Rent | Rent any vehicle, from bikes to cars',
-        description: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!',
-        ogDescription: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!',
-        ogImage: '/public/og_image.png',
-        twitterCard: 'summary_large_image',
+        meta: [
+            { name: 'description', content: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!' },
+            { name: 'og:title', content: 'Omni Rent | Rent any vehicle, from bikes to cars' },
+            { name: 'og:description', content: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!' },
+            { name: 'og:type', content: 'website' },
+            { name: 'og:image', content: 'https://rent-website.netlify.app/og_image.png' },
+            { name: 'og:url', content: 'https://rent-website.netlify.app' },
+            { name: 'og:site_name', content: 'Omni Rent' },
+        ],
     })
 </script>
