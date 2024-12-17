cfeac93a78ee89042b2179df0b2f1dd1b1e5cf7d
jacopo-derrico <derricoj.design@gmail.com>
2024-12-17 12:13:36 +0100

    feat: SEO tags  and og image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,28 +10,34 @@ export default defineNuxtConfig({
     '@nuxtjs/sitemap',
     '@nuxtjs/robots',
   ],
-  runtimeConfig: {
-      mail: {
-        message: {
-          to: 'info@omnirent.io',
-        },
-        smtp: {
-          host: "smtps.aruba.it",
-          port: 465,
-          auth: {
-            user: process.env.NUXT_EMAIL_USERNAME,
-            pass: process.env.NUXT_EMAIL_PASSWORD,
-          },
-        }
-      },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
   },
-  site: { 
-    url: 'https://www.omnirent.io', 
-    name: 'My Awesome Website' 
+  runtimeConfig: {
+    mail: {
+      message: {
+        to: 'info@omnirent.io',
+      },
+      smtp: {
+        host: "smtps.aruba.it",
+        port: 465,
+        auth: {
+          user: process.env.NUXT_EMAIL_USERNAME,
+          pass: process.env.NUXT_EMAIL_PASSWORD,
+        },
+      }
     },
-    robots: {
-      allow: [
-        '*'
-      ]
-    },
+  },
+  site: {
+    url: 'https://www.omnirent.io',
+    name: 'Omni Rent | Rent any vehicle, from bikes to cars'
+  },
+  robots: {
+    allow: [
+      '*'
+    ]
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -6,3 +6,14 @@
         <HomeForm />
     </div>
 </template>
+
+<script setup>
+    useSeoMeta({
+        title: 'Omni Rent | Rent any vehicle, from bikes to cars',
+        ogTitle: 'Omni Rent | Rent any vehicle, from bikes to cars',
+        description: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!',
+        ogDescription: 'Rent your vehicle while you are not using it or rent one when you need it the most. From bikes to cars, rent anything!',
+        ogImage: '/public/og_image.png',
+        twitterCard: 'summary_large_image',
+    })
+</script>
