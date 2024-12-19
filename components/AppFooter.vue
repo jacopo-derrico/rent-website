c093f30ea191d0dc07722d981f1324da173a5c6a
jacopo-derrico <derricoj.design@gmail.com>
2024-12-19 17:59:43 +0100

    feat: update social icons and links

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,9 +2,9 @@
     <div class="bg-[#0174BE] text-center text-white pt-8">
         <div>
             <div class="flex justify-center gap-4 mt-4 w-full">
-                <IconsTwitter :width="'20'" :fillColor="'#fff'"/>
-                <IconsLinkedin :width="'20'" :fillColor="'#fff'"/>
-                <IconsInsta :width="'20'" :fillColor="'#fff'"/>
+                <IconsTwitter :link="twitterLink" :width="'20'" :fillColor="'#fff'"/>
+                <IconsTelegram :link="telegramLink" :width="'20'" :fillColor="'#fff'"/>
+                <IconsInsta :link="instaLink" :width="'20'" :fillColor="'#fff'"/>
             </div>
             <a class="hover:underline hover:text-[#FFC436]" href="mailto:info@omnirent.io">info@omnirent.io</a>
         </div>
@@ -16,4 +16,8 @@
 
 <script setup>
     let currentYear = new Date().getFullYear();
+
+    const twitterLink = 'https://x.com/OMNI_RENT';
+    const instaLink = 'https://www.instagram.com/omni.rent/profilecard/?igsh=MWFxNnYxcDZicG82ZA==';
+    const telegramLink = 'https://t.me/omnirent';
 </script>
